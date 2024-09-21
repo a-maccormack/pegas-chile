1f8c3287cd929aa82f2eb51be67b5e0b2c0f5cac
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-21 20:14:06 -0300

    add herosection

--- frontend/src/containers/Home/heroSection.tsx
+++ frontend/src/containers/Home/heroSection.tsx
@@ -1,6 +1,6 @@
 export const HeroSection = () => {
   return (
-    <header className="mt-16 flex flex-col items-center w-[60%] mx-auto">
+    <header className="mt-10 flex flex-col items-center w-[60%] mx-auto md:mt-32">
       <div className="font-bold border border-gray-300 rounded-lg text-center px-4 py-2 w-fit text-sm">
         Última actualización: 18/09/24
       </div>
@@ -12,6 +12,9 @@ export const HeroSection = () => {
             ¿Que esperas para encontrar el tuyo?
           </span>
         </h1>
+      </div>
+      <div className="mt-8 text-slate-500">
+        <p>Encuentra tu trabajo de software soñado.</p>
       </div>
     </header>
   );
