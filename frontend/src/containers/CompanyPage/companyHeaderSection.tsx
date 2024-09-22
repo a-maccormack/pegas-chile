4d6810443a4a759bdef106a3b6c3aaba36eceeb8
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-22 20:13:23 -0300

    created detailed company view container

--- frontend/src/containers/CompanyPage/companyHeaderSection.tsx
+++ frontend/src/containers/CompanyPage/companyHeaderSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface CompanyHeaderSectionProps {
   companyName: string;
@@ -8,7 +8,7 @@ export const CompanyHeaderSection: React.FC<CompanyHeaderSectionProps> = ({
   companyName,
 }) => {
   return (
-    <div className="text-center text-xl mt-10">
+    <div className="mt-10 text-center text-5xl">
       <h1 className="font-bold">{decodeURIComponent(companyName)}</h1>
     </div>
   );
---
+++ frontend/src/containers/CompanyPage/companyPostsSection.tsx
@@ -0,0 +1,31 @@
+import { JobPost } from "@/app/types";
+import { JobPostCard } from "@/components/jobPostCard";
+import { SalaryChart } from "@/components/salaryChart";
+import { TecnologiesContainer } from "@/components/tecnologiesContainer";
+
+interface SalaryChartSectionProps {
+  companyName: string;
+  companyJobPosts: JobPost[];
+}
+
+export const CompanyPostsSection: React.FC<SalaryChartSectionProps> = ({
+  companyName,
+  companyJobPosts,
+}) => {
+  return (
+    <div className="my-20 flex-row justify-center gap-4 2xl:flex">
+      <div className="mb-4 flex flex-col gap-4 2xl:mx-auto 2xl:w-3/5">
+        <SalaryChart companyName={companyName} jobPosts={companyJobPosts} />
+        <TecnologiesContainer companyName={companyName} />
+      </div>
+      <div className="flex max-h-[550px] flex-col gap-2 overflow-y-auto">
+        {companyJobPosts
+          .slice()
+          .reverse()
+          .map((jobPost, index) => (
+            <JobPostCard key={index} jobPost={jobPost} />
+          ))}
+      </div>
+    </div>
+  );
+};
