f344ec667cdc890fd0393712b6dce96724bc7575
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-22 03:12:22 -0300

    bulk changes

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -1,21 +1,64 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import SearchBar from '../searchBar';
 import Logo from '@/assets/navbar/logo.png';
+import ApiService from '@/services/apiService';
+import Link from 'next/link';
 
 function Navbar() {
+  const apiService = new ApiService();
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (searchTerm) {
+        try {
+          const response = await apiService.searchForCompaniesByTerm(
+            searchTerm
+          );
+          console.log();
+          setSearchResults(response.data.companies);
+        } catch (error) {
+          console.error('Error fetching search results:', error);
+        }
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    const handler = setTimeout(() => {
+      fetchSearchResults();
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchTerm]);
+
+  const handleSearchChange = (term: string) => {
+    setSearchTerm(term);
+  };
+
   return (
     <nav className="bg-black border-gray-200">
       <div className="flex items-center w-[60%] mx-auto py-4 sm:py-0">
         <div className="flex items-center w-fit">
-          <Image className="w-14" src={Logo} alt="Logo" />
-          <div className="w-fit">
-            <h1 className="font-bold text-white hidden sm:block">PegasChile</h1>
-          </div>
+          <Link href="/" className="flex items-center">
+            <Image className="w-14" src={Logo} alt="Logo" />
+            <div className="w-fit">
+              <h1 className="font-bold text-white hidden sm:block">
+                PegasChile
+              </h1>
+            </div>
+          </Link>
         </div>
-        <SearchBar />
+        <SearchBar
+          onChange={handleSearchChange}
+          searchResults={searchResults}
+        />
       </div>
     </nav>
   );
--- frontend/src/components/searchBar.tsx
+++ frontend/src/components/searchBar.tsx
@@ -1,9 +1,22 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useRef } from 'react';
+import Link from 'next/link';
+import React, { useEffect, useRef, useState } from 'react';
 
-const SearchBar: React.FC = () => {
+interface SearchBarProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onChange: (term: string) => void;
+  searchResults: string[];
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  searchResults,
+  onChange,
+  ...props
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [blurTimeout, setBlurTimeout] = useState<NodeJS.Timeout | null>(null);
 
   const focusInput = () => {
     if (inputRef.current) {
@@ -11,10 +24,26 @@ const SearchBar: React.FC = () => {
     }
   };
 
-  const blurInput = () => {
-    if (inputRef.current) {
-      inputRef.current.blur();
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setBlurTimeout(
+      setTimeout(() => {
+        setIsFocused(false);
+      }, 200)
+    );
+  };
+
+  const handleMouseEnter = () => {
+    if (blurTimeout) {
+      clearTimeout(blurTimeout);
     }
+  };
+
+  const handleMouseLeave = () => {
+    handleInputBlur();
   };
 
   useEffect(() => {
@@ -25,7 +54,10 @@ const SearchBar: React.FC = () => {
       }
 
       if (event.key === 'Escape') {
-        blurInput();
+        if (inputRef.current) {
+          inputRef.current.blur();
+        }
+        setIsFocused(false);
       }
     };
 
@@ -33,30 +65,60 @@ const SearchBar: React.FC = () => {
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      if (blurTimeout) {
+        clearTimeout(blurTimeout);
+      }
     };
-  }, []);
+  }, [blurTimeout]);
 
   const handleDivClick = () => {
     focusInput();
   };
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
   return (
-    <div
-      className="ml-4 flex items-center gap-2 bg-gray-900 px-2 rounded-lg cursor-text"
-      onClick={handleDivClick}
-    >
-      <FontAwesomeIcon className="text-white h-4" icon={faSearch} />
-      <input
-        ref={inputRef}
-        className="rounded h-8 text-xs w-full pl-4 bg-transparent text-white outline-none"
-        placeholder="Busca empresa por nombre"
-      />
-      <div className="text-white rounded bg-black w-4 text-center hidden sm:block">
-        ⌘
+    <div className="relative">
+      <div
+        className="ml-4 flex items-center gap-2 bg-gray-900 px-2 rounded-lg cursor-text"
+        onClick={handleDivClick}
+      >
+        <FontAwesomeIcon className="text-white h-4" icon={faSearch} />
+        <input
+          ref={inputRef}
+          className="rounded h-8 text-xs w-full pl-4 bg-transparent text-white outline-none"
+          placeholder="Busca empresa por nombre"
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          {...props}
+        />
+        <div className="text-white rounded bg-black w-4 text-center hidden sm:block">
+          ⌘
+        </div>
+        <div className="text-white rounded bg-black w-4 text-center hidden sm:block">
+          k
+        </div>
       </div>
-      <div className="text-white rounded bg-black w-4 text-center hidden sm:block">
-        k
-      </div>
+      {isFocused && searchResults.length > 0 && (
+        <div
+          className="absolute ml-4 w-[calc(100%-1rem)] bg-black z-10"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {searchResults.map((result, index) => (
+            <Link
+              href={`/company/${result}`}
+              key={index}
+              className="text-white p-2 hover:bg-gray-800 block visited:text-white visited:no-underline"
+            >
+              {result}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
