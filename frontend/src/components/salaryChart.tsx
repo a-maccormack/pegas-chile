3a107296d17aef6befdc012162926249dc28ef57
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-22 12:22:32 -0300

    add chart range options

--- frontend/src/components/salaryChart.tsx
+++ frontend/src/components/salaryChart.tsx
@@ -23,24 +23,56 @@ export const SalaryChart: React.FC<SalaryChartProps> = ({
   const [chartData, setChartData] = useState<
     { date: string; salary: number | null }[]
   >([]);
+  const [salaryType, setSalaryType] = useState<'min' | 'middle' | 'upper'>(
+    'min'
+  );
 
   useEffect(() => {
     const data = jobPosts.map((jobPost: JobPost) => {
-      const salary = parseInt(jobPost.salary_range.min_bound) / 1000000;
+      const minSalary = parseInt(jobPost.salary_range.min_bound) / 1000000;
+      const upperSalary = parseInt(jobPost.salary_range.max_bound) / 1000000;
+      let salary: number | null = null;
+
+      if (salaryType === 'min') {
+        salary = isNaN(minSalary) || minSalary === 0 ? null : minSalary;
+      } else if (salaryType === 'middle') {
+        const middleSalary = (minSalary + upperSalary) / 2;
+        salary =
+          isNaN(middleSalary) || middleSalary === 0 ? null : middleSalary;
+      } else if (salaryType === 'upper') {
+        salary = isNaN(upperSalary) || upperSalary === 0 ? null : upperSalary;
+      }
+
       return {
         date: `${jobPost.date.day}/${jobPost.date.month}/${jobPost.date.year}`,
-        salary: isNaN(salary) || salary === 0 ? null : salary,
+        salary: salary,
       };
     });
 
-    setChartData((prev) => [...prev, ...data]);
-  }, [jobPosts]);
+    console.log(data);
+    setChartData(data);
+  }, [jobPosts, salaryType]);
 
   return (
     <div className="mt-20">
       {jobPosts.length > 0 ? (
         <div className="text-center mx-auto pb-10 h-40 sm:h-96 w-full">
           <h3 className="my-10 font-bold">Sueldos Historicos {companyName}</h3>
+          <div className="mb-5">
+            <label className="mr-2">Selecciona el tipo de sueldo:</label>
+            <select
+              value={salaryType}
+              onChange={(e) =>
+                setSalaryType(e.target.value as 'min' | 'middle' | 'upper')
+              }
+              className="border rounded px-3 py-2"
+            >
+              <option value="min">Franja Inferior</option>
+              <option value="middle">Promedio (Mín y Máx)</option>
+              <option value="upper">Franja superior</option>
+            </select>
+          </div>
+
           <ResponsiveContainer width="100%" height="100%">
             <LineChart data={chartData}>
               <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
