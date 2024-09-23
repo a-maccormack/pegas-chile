0288b6573be7c0a971affb2128dce4b250d0ecdb
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 00:14:35 -0300

    add id retrieval api routes

---
+++ frontend/src/app/api/search/technologiesByPostId/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { JobPost } from '@/app/types';
+import data from '@/job-posts.json';
+
+export const runtime = 'edge';
+
+const jobPosts = data as JobPost[];
+
+const getJobPostById = (id: number): JobPost | {} => {
+    if (id < 0 || id >= jobPosts.length) {
+        return {};
+    }
+    return jobPosts[id - 1].technologies;
+};
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const jobId = searchParams.get('id');
+
+    if (!jobId) {
+        return NextResponse.json({ error: 'Job ID is required' }, { status: 400 });
+    }
+
+    const parsedJobId = parseInt(jobId, 10);
+    const results = getJobPostById(parsedJobId);
+
+    return NextResponse.json(results);
+}
