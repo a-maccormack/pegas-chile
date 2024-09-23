a3e7d03bcea74a34d29f252b7ebcf718469fdd1b
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 08:28:26 -0300

    tailwind order formatting

--- frontend/src/containers/Home/heroSection.tsx
+++ frontend/src/containers/Home/heroSection.tsx
@@ -1,11 +1,11 @@
 export const HeroSection = () => {
   return (
-    <header className="mt-10 flex flex-col items-center w-[60%] mx-auto md:mt-32">
-      <div className="font-bold border border-gray-300 rounded-lg text-center px-4 py-2 w-fit text-sm">
+    <header className="mx-auto mt-10 flex w-[60%] flex-col items-center md:mt-32">
+      <div className="w-fit rounded-lg border border-gray-300 px-4 py-2 text-center text-sm font-bold">
         Última actualización: 18/09/24
       </div>
       <div className="mt-10 text-center font-extrabold lg:max-w-5xl">
-        <h1 className="text-lg md:text-3xl lg:text-5xl">
+        <h1 className="text-2xl md:text-3xl lg:text-5xl">
           Más de <span className="text-green-600">1400</span> publicaciones de
           trabajos.
           <span className="text-green-600">
