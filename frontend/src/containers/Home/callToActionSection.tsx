9ecb74292d4a8ba70a842ba32920a8b145064e8e
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 15:21:56 -0300

    add companies button

--- frontend/src/containers/Home/callToActionSection.tsx
+++ frontend/src/containers/Home/callToActionSection.tsx
@@ -1,11 +1,21 @@
+import { faBriefcase, faBuilding } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 
 export const CallToActionSection = () => {
   return (
-    <div className="mx-auto my-8 w-fit">
+    <div className="mx-auto my-8 flex w-fit flex-col items-center gap-4 sm:flex-row">
       <Link href="/jobs" className="rounded-lg bg-green-600 p-4 text-white">
-        Ver Pegas
+        <FontAwesomeIcon icon={faBriefcase} />
+        <span className="ml-2">Ver Trabajos</span>
+      </Link>
+      <Link
+        href="/companies"
+        className="rounded-lg bg-green-600 p-4 text-white"
+      >
+        <FontAwesomeIcon icon={faBuilding} />
+        <span className="ml-2">Ver Empresas</span>
       </Link>
     </div>
   );
