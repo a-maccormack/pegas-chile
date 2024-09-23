4b305da7c85933541db3710e514a3339ca4e08e7
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 10:23:21 -0300

    add SEO section

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,11 +3,20 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
+import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const metaData = {
   title: "Pegas Chile",
   description: "Más de 1400 publicaciones de trabajos de software en Chile.",
+  url: "https://pegas-chile.vercel.app",
+  imageUrl: "https://pegas-chile.vercel.app/banner.png",
+};
+
+export const metadata: Metadata = {
+  title: metaData.title,
+  description: metaData.description,
 };
 
 export default function RootLayout({
@@ -17,6 +26,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <>
+        <Head>
+          {/* Browser */}
+          <title>{metaData.title}</title>
+          <meta name="description" content={metaData.description} />
+
+          {/* OpenGraph */}
+          <meta property="og:title" content={metaData.title} />
+          <meta property="og:description" content={metaData.description} />
+          <meta property="og:image" content={metaData.imageUrl} />
+          <meta property="og:url" content={metaData.url} />
+          <meta property="og:type" content="article" />
+
+          {/* Twitter */}
+          <meta name="twitter:title" content={metaData.title} />
+          <meta name="twitter:description" content={metaData.description} />
+          <meta name="twitter:image" content={metaData.imageUrl} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+      </>
       <body className={inter.className}>
         <Navbar />
         {children}
