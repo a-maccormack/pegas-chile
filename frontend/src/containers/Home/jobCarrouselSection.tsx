aa84b52fecd107ce3c1ba10ed99674bb53a8e138
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 08:28:38 -0300

    hide on mobile

--- frontend/src/containers/Home/jobCarrouselSection.tsx
+++ frontend/src/containers/Home/jobCarrouselSection.tsx
@@ -1,37 +1,37 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Container from '@/components/container';
-import fintualLogo from '@/assets/job-carrousel/fintual-logo.png';
-import cornerShopLogo from '@/assets/job-carrousel/cornershop-logo.png';
-import machLogo from '@/assets/job-carrousel/mach-logo.png';
-import mercadolibreLogo from '@/assets/job-carrousel/mercadolibre-logo.png';
-import { CompanyCard } from '@/components/companyCard';
+import React, { useState } from "react";
+import Container from "@/components/container";
+import fintualLogo from "@/assets/job-carrousel/fintual-logo.png";
+import cornerShopLogo from "@/assets/job-carrousel/cornershop-logo.png";
+import machLogo from "@/assets/job-carrousel/mach-logo.png";
+import mercadolibreLogo from "@/assets/job-carrousel/mercadolibre-logo.png";
+import { CompanyCard } from "@/components/companyCard";
 
 const logoData = [
-  { src: fintualLogo, alt: 'fintual logo', salary: '+$ 1M' },
-  { src: cornerShopLogo, alt: 'cornershop logo', salary: '+$ 2M' },
-  { src: machLogo, alt: 'mach logo', salary: '+$ 2M' },
-  { src: mercadolibreLogo, alt: 'mercadolibre logo', salary: '+$ 1M' },
+  { src: fintualLogo, alt: "fintual logo", salary: "+$ 1M" },
+  { src: cornerShopLogo, alt: "cornershop logo", salary: "+$ 2M" },
+  { src: machLogo, alt: "mach logo", salary: "+$ 2M" },
+  { src: mercadolibreLogo, alt: "mercadolibre logo", salary: "+$ 1M" },
 ];
 
 function JobCarrousel() {
   return (
-    <div className="bg-carrousel-gray mt-12 py-8">
-      <Container className="md:flex items-center lg:max-w-5xl flex-row overflow-hidden">
+    <div className="mt-12 hidden bg-carrousel-gray py-8 sm:block">
+      <Container className="flex-row items-center overflow-hidden md:flex lg:max-w-5xl">
         <div>
           <h2 className="font-extrabold">Están esperandote!</h2>
           <p className="text-gray-500">Negocia tu sueldo</p>
         </div>
 
-        <div className="relative overflow-hidden w-full group">
-          <div className="hidden md:block absolute left-0 top-0 h-full w-24 bg-gradient-to-r from-carrousel-gray to-transparent pointer-events-none z-10"></div>
-          <div className="hidden md:block absolute right-0 top-0 h-full w-24 bg-gradient-to-l from-carrousel-gray to-transparent pointer-events-none z-10"></div>
+        <div className="group relative w-full overflow-hidden">
+          <div className="pointer-events-none absolute left-0 top-0 z-10 hidden h-full w-24 bg-gradient-to-r from-carrousel-gray to-transparent md:block"></div>
+          <div className="pointer-events-none absolute right-0 top-0 z-10 hidden h-full w-24 bg-gradient-to-l from-carrousel-gray to-transparent md:block"></div>
           <div className="w-full">
-            <div className="inline-flex whitespace-nowrap items-center md:gap-10 animate-none md:animate-loop-scroll group-hover:paused">
+            <div className="group-hover:paused inline-flex animate-none items-center whitespace-nowrap md:animate-loop-scroll md:gap-10">
               {[...logoData, ...logoData, ...logoData].map((logo, index) => (
                 <CompanyCard
-                  className={index > 1 ? 'hidden' : ''}
+                  className={index > 1 ? "hidden" : ""}
                   key={index}
                   src={logo.src}
                   alt={logo.alt}
