4aab19ce13d5ee77f8cf5e91f2aa46b898b9b789
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-09-23 08:27:50 -0300

    change to normal export

--- frontend/src/containers/Home/callToActionSection.tsx
+++ frontend/src/containers/Home/callToActionSection.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import React from "react";
 
-const CallToActionSection = () => {
+export const CallToActionSection = () => {
   return (
-    <div className="mx-auto mt-8 w-fit">
+    <div className="mx-auto my-8 w-fit">
       <Link href="/jobs" className="rounded-lg bg-green-600 p-4 text-white">
         Ver Pegas
       </Link>
     </div>
   );
 };
-
-export default CallToActionSection;
