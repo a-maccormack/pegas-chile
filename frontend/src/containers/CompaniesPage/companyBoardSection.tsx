be8d0f60f4b56c68d468bdd97ec1506ba6cfa07b
nabenitez <nabenitez@uc.cl>
2024-10-01 13:52:23 -0300

    feat(frontend): use infinite scroll in companies

--- frontend/src/containers/CompaniesPage/companyBoardSection.tsx
+++ frontend/src/containers/CompaniesPage/companyBoardSection.tsx
@@ -1,98 +1,80 @@
 "use client";
 
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 // TODO: make into a common component
 import ApiService from "@/services/apiService";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 export const CompanyBoardSection = () => {
   const apiService = new ApiService();
-  const router = useRouter();
 
-  const searchParams = useSearchParams();
-  const pageParam = searchParams.get("page") ?? "1";
-
-  const [page, setPage] = useState<number>(parseInt(pageParam));
-  const [companies, setCompanies] = useState<string[] | null>(null);
+  const [companies, setCompanies] = useState<string[]>([]);
+  const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCompanies = async () => {
+    setLoading(true);
+    try {
+      const response = await apiService.getCompanies(page);
+      setCompanies((previousCompanies) => [
+        ...previousCompanies,
+        ...response.data.companies,
+      ]);
+      setTotalPages(response.data.totalPages);
+    } catch (error) {
+      console.error("Error fetching companies posts:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    apiService
-      .getCompanies(page)
-      .then((response) => {
-        setCompanies(response.data.companies);
-        setTotalPages(response.data.totalPages);
-      })
-      .catch((error) => {
-        console.error("Error fetching companies:", error);
-      });
-  }, [page, pageParam]);
+    console.log("executing-use-effect");
+    console.log(page);
+    fetchCompanies();
+  }, [page]);
 
-  const handleNextPage = () => {
-    if (page < totalPages) {
-      const newPage = page + 1;
-      setPage(newPage);
-      router.push(`?page=${newPage}`);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (page > 1) {
-      const newPage = page - 1;
-      setPage(newPage);
-      router.push(`?page=${newPage}`);
-    }
-  };
+  const { loadMoreRef } = useInfiniteScroll({
+    totalPages,
+    loading,
+    onLoadMore: () => setPage((prevPage) => prevPage + 1),
+  });
 
   return (
     <>
-      <div className="my-5 flex items-center justify-between">
-        <button
-          className={`rounded bg-green-600 px-4 py-2 text-white ${
-            page === 1 ? "cursor-not-allowed opacity-50" : ""
-          }`}
-          onClick={handlePreviousPage}
-          disabled={page === 1}
-        >
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-
-        <span className="text-xl">
-          Página {page} de {totalPages}
-        </span>
-        <button
-          className={`rounded bg-green-600 px-4 py-2 text-white ${
-            page === totalPages ? "cursor-not-allowed opacity-50" : ""
-          }`}
-          onClick={handleNextPage}
-          disabled={page === totalPages}
-        >
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
-
       <div
-        className="mb-20 flex flex-col gap-5 overflow-y-auto"
+        className="my-16 flex flex-col gap-5 overflow-y-auto"
         style={{ maxHeight: "calc(100vh - 350px)" }}
       >
-        {companies && companies.length > 0 ? (
-          companies.map((company, index) => (
-            <Link
-              key={index}
-              href={`/company/${company}`}
-              className="w-full rounded-md border border-gray-200 py-2 hover:bg-carrousel-gray"
-            >
-              <div className="ml-2">{company}</div>
-            </Link>
-          ))
-        ) : (
-          <p>Loading companies...</p>
-        )}
+        <CompaniesList companies={companies} />
+        <div ref={loadMoreRef}>
+          <LoadingIndicator loading={loading} />
+        </div>
       </div>
     </>
   );
 };
+
+//TODO: move to own component
+const CompaniesList = ({ companies }: { companies: string[] }) => (
+  <>
+    {companies.length > 0 &&
+      companies.map((company, index) => (
+        <Link
+          key={index}
+          href={`/company/${company}`}
+          className="h-40 w-full rounded-md border border-gray-200 py-2 hover:bg-carrousel-gray"
+        >
+          <div className="ml-2">{company}</div>
+        </Link>
+      ))}
+  </>
+);
+
+const LoadingIndicator = ({ loading }: { loading: boolean }) => (
+  <div className="flex h-10 w-full items-center justify-center">
+    {loading && <p>Loading more companies...</p>}
+  </div>
+);
