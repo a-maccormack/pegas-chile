49d557a6b0d285973e15d1fd6ced483a8cd20664
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2025-04-08 08:46:54 -0400

    update latest job posts

--- frontend/src/containers/Home/heroSection.tsx
+++ frontend/src/containers/Home/heroSection.tsx
@@ -1,21 +1,21 @@
 export const HeroSection = () => {
-  return (
-    <header className="mx-auto mt-10 flex w-[60%] flex-col items-center md:mt-32">
-      <div className="w-fit rounded-lg border border-gray-300 px-4 py-2 text-center text-sm font-bold">
-        Última actualización: 28/10/24
-      </div>
-      <div className="mt-10 text-center font-extrabold lg:max-w-5xl">
-        <h1 className="text-2xl md:text-3xl lg:text-5xl">
-          Más de <span className="text-green-600">1400</span> publicaciones de
-          trabajos.
-          <span className="text-green-600">
-            ¿Que esperas para encontrar el tuyo?
-          </span>
-        </h1>
-      </div>
-      <div className="mt-8 text-slate-500">
-        <p>Encuentra tu trabajo de software soñado.</p>
-      </div>
-    </header>
-  );
+    return (
+        <header className="mx-auto mt-10 flex w-[60%] flex-col items-center md:mt-32">
+            <div className="w-fit rounded-lg border border-gray-300 px-4 py-2 text-center text-sm font-bold">
+                Última actualización: 08/04/25
+            </div>
+            <div className="mt-10 text-center font-extrabold lg:max-w-5xl">
+                <h1 className="text-2xl md:text-3xl lg:text-5xl">
+                    Más de <span className="text-green-600">1400</span> publicaciones de
+                    trabajos.
+                    <span className="text-green-600">
+                        ¿Que esperas para encontrar el tuyo?
+                    </span>
+                </h1>
+            </div>
+            <div className="mt-8 text-slate-500">
+                <p>Encuentra tu trabajo de software soñado.</p>
+            </div>
+        </header>
+    );
 };
